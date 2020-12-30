1465f8a4312ce6fa175ee2dc92198d8bdac09641
hd-chun <65650495+hd-chun@users.noreply.github.com>
2020-12-30 14:34:08 +0800

    Update config.php

--- application/config.php
+++ application/config.php
@@ -155,8 +155,8 @@ return [
     'error_message'          => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'         => false,
-    // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'       => 'app\lib\exception\ExceptionHandler',
+    // 异常处理handle类 留空使用 
+    'exception_handle'       => '',
 
     // +----------------------------------------------------------------------
     // | 日志设置
